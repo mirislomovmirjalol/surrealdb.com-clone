61ebfb346f013b35aa578e4c93fb719087c41c1d
mirjalol <mirislomovmirjalol@gmail.com>
2023-08-22 18:40:18 +0100

    drop down with icon

--- app/components/UI/NavigationBar.tsx
+++ app/components/UI/NavigationBar.tsx
@@ -17,12 +17,14 @@ import {SurrealLogo} from "@/app/components/UI/SurrealLogo";
 import NavLink from "@/app/components/UI/NavLink";
 import NeonButton from "@/app/components/UI/NeonButton";
 import WaitingListModal from "@/app/components/UI/WaitingListModal";
+import Link from "next/link";
 
 type MenuListType = {
     title: string
     path?: string
     menuItems?: MenuListType[]
     description?: string,
+    icon?: React.ReactNode
 }
 
 type NavDropdownMenuProps = {
@@ -56,8 +58,13 @@ const NavDropdownMenu = (props: NavDropdownMenuProps) => {
                         className="p-4"
                         key={index}
                         description={item.description}
+                        startContent={item.icon}
                     >
-                        {item.title}
+                        {item.path &&
+                            <Link href={item.path}>
+                                {item.title}
+                            </Link>
+                        }
                     </DropdownItem>
                 ))}
             </DropdownMenu>
@@ -84,24 +91,38 @@ export default function NavigationBar() {
             title: "Product",
             menuItems: [
                 {
-                    title: "Community",
-                    path: "/community",
-                    description: "Join the community and get help from other users.",
+                    title: "Why SurrealDB?",
+                    path: "/about",
+                    description: "Understand why SurrealDB is the ultimate cloud database.",
+                    icon: <svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" strokeWidth={1.5} stroke="currentColor" className="w-6 h-6">
+                        <path strokeLinecap="round" strokeLinejoin="round" d="M9.879 7.519c1.171-1.025 3.071-1.025 4.242 0 1.172 1.025 1.172 2.687 0 3.712-.203.179-.43.326-.67.442-.745.361-1.45.999-1.45 1.827v.75M21 12a9 9 0 11-18 0 9 9 0 0118 0zm-9 5.25h.008v.008H12v-.008z" />
+                    </svg>,
                 },
                 {
-                    title: "product",
-                    path: "/product",
-                    description: "bla kjsadjfla klsdjfj asjljsdkf"
+                    title: "Features",
+                    path: "/features",
+                    description: "Get a detailed overview of all of SurrealDB's technical features.",
+                    icon: <svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" strokeWidth={1.5} stroke="currentColor" className="w-6 h-6">
+                        <path strokeLinecap="round" strokeLinejoin="round" d="M9 12h3.75M9 15h3.75M9 18h3.75m3 .75H18a2.25 2.25 0 002.25-2.25V6.108c0-1.135-.845-2.098-1.976-2.192a48.424 48.424 0 00-1.123-.08m-5.801 0c-.065.21-.1.433-.1.664 0 .414.336.75.75.75h4.5a.75.75 0 00.75-.75 2.25 2.25 0 00-.1-.664m-5.8 0A2.251 2.251 0 0113.5 2.25H15c1.012 0 1.867.668 2.15 1.586m-5.8 0c-.376.023-.75.05-1.124.08C9.095 4.01 8.25 4.973 8.25 6.108V8.25m0 0H4.875c-.621 0-1.125.504-1.125 1.125v11.25c0 .621.504 1.125 1.125 1.125h9.75c.621 0 1.125-.504 1.125-1.125V9.375c0-.621-.504-1.125-1.125-1.125H8.25zM6.75 12h.008v.008H6.75V12zm0 3h.008v.008H6.75V15zm0 3h.008v.008H6.75V18z" />
+                    </svg>
+
                 },
                 {
-                    title: "Community",
-                    path: "/community",
-                    description: "Join the community and get help from other users.",
+                    title: "Release ",
+                    path: "/release",
+                    description: "Keep track of the notable updates and improvements for every\n" +
+                        "SurrealDB product release in chronological order.",
+                    icon: <svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" strokeWidth={1.5} stroke="currentColor" className="w-6 h-6">
+                        <path strokeLinecap="round" strokeLinejoin="round" d="M15.59 14.37a6 6 0 01-5.84 7.38v-4.8m5.84-2.58a14.98 14.98 0 006.16-12.12A14.98 14.98 0 009.631 8.41m5.96 5.96a14.926 14.926 0 01-5.841 2.58m-.119-8.54a6 6 0 00-7.381 5.84h4.8m2.581-5.84a14.927 14.927 0 00-2.58 5.84m2.699 2.7c-.103.021-.207.041-.311.06a15.09 15.09 0 01-2.448-2.448 14.9 14.9 0 01.06-.312m-2.24 2.39a4.493 4.493 0 00-1.757 4.306 4.493 4.493 0 004.306-1.758M16.5 9a1.5 1.5 0 11-3 0 1.5 1.5 0 013 0z" />
+                    </svg>
                 },
                 {
-                    title: "product",
-                    path: "/product",
-                    description: "bla kjsadjfla klsdjfj asjljsdkf"
+                    title: "Roadmap",
+                    path: "/roadmap",
+                    description: "See what we have planned for SurrealDB, and see the features and functionality which has already been implemented.",
+                    icon: <svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" strokeWidth={1.5} stroke="currentColor" className="w-6 h-6">
+                        <path strokeLinecap="round" strokeLinejoin="round" d="M9 6.75V15m6-6v8.25m.503 3.498l4.875-2.437c.381-.19.622-.58.622-1.006V4.82c0-.836-.88-1.38-1.628-1.006l-3.869 1.934c-.317.159-.69.159-1.006 0L9.503 3.252a1.125 1.125 0 00-1.006 0L3.622 5.689C3.24 5.88 3 6.27 3 6.695V19.18c0 .836.88 1.38 1.628 1.006l3.869-1.934c.317-.159.69-.159 1.006 0l4.994 2.497c.317.158.69.158 1.006 0z" />
+                    </svg>
                 },
             ]
         },
@@ -162,7 +183,7 @@ export default function NavigationBar() {
             <NavbarMenu className="backdrop-blur-lg bg-base/30 mt-8">
                 {menuItems.map((item, index) => (
                     item.menuItems ?
-                        <NavDropdownMenu key={index} menuItems={item.menuItems} item={item}/>
+                        <NavDropdownMenu menuItems={item.menuItems} item={item} key={index}/>
                         :
                         (item.path &&
                             <NavbarMenuItem key={index} onClick={() => setIsMenuOpen(!isMenuOpen)}
